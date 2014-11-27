1c8842cda2db5f3f9030169eeed9b30d61274cf3
Xavier Riley <xavriley@hotmail.com>
2014-11-27 10:49:05 +0000

    AJAX form interaction working with sample json for OC api company lookup

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,15 @@
 class PeopleController < ApplicationController
-   def new
-      @person = Person.new  
-   end
+  def new
+    @person = Person.new  
+  end
+
+  def create
+    @person = Person.find_or_create_by(person_params)
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :nationality, :date_of_birth, :address)
+  end
 end

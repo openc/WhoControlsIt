df96938636df57ec74d3748e1004a7d7ae162523
Xavier Riley <xavriley@hotmail.com>
2014-11-28 14:52:05 +0000

    Fix graphs for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,7 +9,26 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def graph_relationships
+    @company = Company.find(params[:id])
+    @graph = @company.graph_relationships
+
+    render :json => @graph
+  end
+
   def show
     @company = Company.find(params[:id])
+  end
+
+  def create
+    @company = Company.find_or_create_by(company_params)
+    @company.save
+    # We're assuming one workflow for creating companies for now
+    redirect_to new_control_relationship_path(child_id: @company.id, child_type: @company.class.to_s)
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :company_number, :jurisdiction_code)
   end
 end

7477fd4eb3099acf6d6ab9adb62250f458140c34
Xavier Riley <xavriley@hotmail.com>
2014-11-28 14:25:43 +0000

    Add S3 config vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,9 @@
 CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   :provider               => 'AWS',                        # required
-  #   :aws_access_key_id      => 'xxx',                        # required
-  #   :aws_secret_access_key  => 'yyy',                        # required
-  #   :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-  #   :host                   => 's3.example.com',             # optional, defaults to nil
-  #   :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  # }
-  # config.fog_directory  = 'name_of_directory'                          # required
-  # config.fog_public     = false                                        # optional, defaults to true
-  # config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['S3_ACCESS'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+  }
+  config.fog_directory  = 'whocontrolsit'                          # required
+  config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
 end

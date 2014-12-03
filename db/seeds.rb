bbd3f77307026afd762f62c8be7852f191f4bff9
Xavier Riley <xavriley@hotmail.com>
2014-12-03 20:51:48 +0000

    Start adding Monsoon wseeds to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Person.delete_all
+Company.delete_all
+ControlRelationship.delete_all
+
+#people = Person.create()
+mal = Company.create({name: "Monsoon Accessorize Limited",
+                      jurisdiction_code: "gb",
+                      company_number: "01098034"})
+mhl = Company.create({name: "Monsoon Holdings Ltd",
+                      jurisdiction_code: "gb",
+                      company_number: "01098034"})
+
+relationships = ControlRelationship.create([
+  {parent: mhl, child: mal, relationship_type: "Shareholding", details: "100"}
+])

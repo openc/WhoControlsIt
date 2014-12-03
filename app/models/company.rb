33f770f9ea38da9ce838669d98e64c499a8032ae
Xavier Riley <xavriley@hotmail.com>
2014-12-03 10:27:29 +0000

    First go at plain english 'Who controls' sentence

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,15 @@ class Company < ActiveRecord::Base
   def opencorporates_url
     "https://OpenCorporates.com/companies/#{jurisdiction_code}/#{company_number}"
   end
+
+  def beneficial_owner_name
+    ultimate_parent_id = get_parent_relationships_for_node(self)[:nodes].select {|x|
+      x[:type] = "Person"
+    }.last[:id][/\d+/]
+    Person.find(ultimate_parent_id).name
+  end
+
+  def beneficial_owner_sentence
+    "#{self.name} is controlled by #{self.beneficial_owner_name}"
+  end
 end

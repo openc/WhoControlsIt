46630e2befc8cd10ba50005651f2702da8d9b84a
Countculture <countculture@gmail.com>
2014-12-07 13:17:00 +0000

    Tweak seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ Person.delete_all
 Company.delete_all
 ControlRelationship.delete_all
 
+# seeds_data = YAML.loadfile('seeds.yaml')
+
 #people = Person.create()
 mal = Company.create({name: "Monsoon Accessorize Limited",
                       jurisdiction_code: "gb",
@@ -12,7 +14,13 @@ mal = Company.create({name: "Monsoon Accessorize Limited",
 mhl = Company.create({name: "Monsoon Holdings Ltd",
                       jurisdiction_code: "gb",
                       company_number: "01098034"})
+ps = Person.create({name: 'Peter Michael Simon',
+                    date_of_birth: "1949-08-04",
+                    address: "15 CHEYNE ROW, LONDON SW3 5TW"})
 
-relationships = ControlRelationship.create([
+ControlRelationship.create([
   {parent: mhl, child: mal, relationship_type: "Shareholding", details: "100"}
+])
+ControlRelationship.create([
+  {parent: ps, child: mhl, relationship_type: "Trust Beneficiary"}
 ])

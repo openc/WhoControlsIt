6033f2b87d1aef26f9e4cc12952ffb717467453c
Countculture <countculture@gmail.com>
2014-12-07 22:47:37 +0000

    Remove People/Companies controllers.
    Functionality now provided by entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,4 +1,5 @@
 class EntitiesController < ApplicationController
+  autocomplete :entity, :name
 
   def create
     @entity = Entity.find_or_create_by(entity_params)
@@ -6,6 +7,22 @@ class EntitiesController < ApplicationController
     # We're assuming one workflow for creating companies for now
     # redirect_to new_control_relationship_path(child_id: @company.id, child_type: @company.class.to_s)
     redirect_to new_control_relationship_path(parent_id: @entity.id)
+  end
+
+  def graph_relationships
+    @entity = Entity.find(params[:id])
+    # @child_relationships = @entity.child_relationships
+    @graph = @entity.graph_relationships
+
+    render :json => @graph
+  end
+
+  def index
+    if params[:name]
+      @entities = Entity.where(:name => params[:name]).page(params[:page])
+    else
+      @entities = Entity.order('created_at DESC').page(params[:page])
+    end
   end
 
   def new
@@ -16,6 +33,7 @@ class EntitiesController < ApplicationController
 
   def show
     @entity = Entity.find(params[:id])
+    @child_relationships = @entity.child_relationships
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-Person.delete_all
-Company.delete_all
+Entity.delete_all
 ControlRelationship.delete_all
 
 # seeds_data = YAML.loadfile('seeds.yaml')
 
-#people = Person.create()
-# mal = Company.create({name: "Monsoon Accessorize Limited",
-#                       jurisdiction: "gb",
-#                       company_number: "01098034"})
-# mhl = Company.create({name: "Monsoon Holdings Ltd",
-#                       jurisdiction: "gb",
-#                       company_number: "01098034"})
-# ps = Person.create({name: 'Peter Michael Simon',
-#                     date_of_birth: "1949-08-04",
-#                     address: "15 CHEYNE ROW, LONDON SW3 5TW"})
-#
-# ControlRelationship.create([
-#   {parent: mhl, child: mal, relationship_type: "Shareholding", details: "100"}
-# ])
-# ControlRelationship.create([
-#   {parent: ps, child: mhl, relationship_type: "Trust Beneficiary"}
-# ])
+mal = Entity.create({name: "Monsoon Accessorize Limited",
+                      jurisdiction: "gb",
+                      company_number: "01098034",
+                      entity_type: 'Company'})
+mhl = Entity.create({name: "Monsoon Holdings Ltd",
+                      jurisdiction: "gb",
+                      company_number: "01098034",
+                      entity_type: 'Company'})
+ps = Entity.create({  name: 'Peter Michael Simon',
+                      date_of_birth: "1949-08-04",
+                      address: "15 CHEYNE ROW, LONDON SW3 5TW",
+                      entity_type: 'Person'})
+
+ControlRelationship.create([
+  {parent: mhl, child: mal, relationship_type: "Shareholding", details: "100"}
+])
+ControlRelationship.create([
+  {parent: ps, child: mhl, relationship_type: "Trust Beneficiary"}
+])

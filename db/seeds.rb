0cb4d7a1848df99b5d91eca6a8d956944a0fea5a
Countculture <countculture@gmail.com>
2014-12-07 23:07:19 +0000

    Add ct/chrinon to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,25 @@ mhl = Entity.create({name: "Monsoon Holdings Ltd",
                       company_number: "01098034",
                       entity_type: 'Company'})
 ps = Entity.create({  name: 'Peter Michael Simon',
-                      date_of_birth: "1949-08-04",
+                      date_of_birth: "1949-08-01",
                       address: "15 CHEYNE ROW, LONDON SW3 5TW",
                       entity_type: 'Person'})
+ct = Entity.create({  name: 'Chris Taggart',
+                      date_of_birth: "1964-04-01",
+                      address: "Aston House, Cornwall Avenue, London N3 1LF",
+                      entity_type: 'Person',
+                      jurisdiction: 'British'})
+chr = Entity.create({ name: 'Chrinon Ltd',
+                      jurisdiction: "gb",
+                      company_number: "07444723",
+                      entity_type: 'Company'})
 
 ControlRelationship.create([
   {parent: mhl, child: mal, relationship_type: "Shareholding", details: "100"}
 ])
 ControlRelationship.create([
   {parent: ps, child: mhl, relationship_type: "Trust Beneficiary"}
+])
+ControlRelationship.create([
+  {parent: ct, child: chr, relationship_type: "Shareholding", details: "90"}
 ])

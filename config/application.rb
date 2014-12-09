1b69e90a3d3958b2f42eb2a4971a872b779c21e8
atmostat <aeropoetics@gmail.com>
2014-12-09 14:36:20 +0000

    Attempting heroku fix

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Whocontrolsit
   class Application < Rails::Application
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

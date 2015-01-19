1930811fc424f5d5721cda97aa648e108bfb0f45
Dan O'Huiginn <daniel@ohuiginn.net>
2015-01-19 13:42:37 +0100

    Base jurisdiction on opencorporates jurisdiction list
    Partially fixes issue #15

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,9 +1,12 @@
+
+
 class Entity < ActiveRecord::Base
   # :child_relationships are ones where the entity is parent, and the relationships are with children
   has_many :child_relationships, :class_name => "ControlRelationship", :foreign_key => "parent_id"
   # ::parent_relationships are ones where the entity is child, and the relationships are with parents
   has_many :parent_relationships, :class_name => "ControlRelationship", :foreign_key => "child_id"
   validates_presence_of :name, :entity_type
+  validates :jurisdiction, :allow_nil => true, :inclusion => { in: Rails.application.config.countries.collect{ |x| x[1]}}
 
 
   def initialize_graph
@@ -102,4 +105,8 @@ class Entity < ActiveRecord::Base
     }
   end
 
+
+  def jurisdiction_name()
+    return Hash[Rails.application.config.countries].invert[jurisdiction]
+  end
 end
